3902150eb0305d9d04419cec4850336b4b585aa6
vm_ubuntu42 <dewitskibenji@live.fr>
2025-01-18 08:20:44 -0800

    add makefile, small norm, forgotten wrong*.cpp

---
+++ ex00/WrongAnimal.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongAnimal.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/18 08:17:15 by bde-wits          #+#    #+#             */
+/*   Updated: 2025/01/18 08:17:34 by bde-wits         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "WrongAnimal.hpp"
+
+std::string	WrongAnimal::getType() const
+{
+	return (this->type);
+}
+
+void	WrongAnimal::makeSound() const
+{
+	std::cout << "is magic , uuuhhh uuuhhh" << std::endl;
+}
+
+WrongAnimal::WrongAnimal()
+{
+	this->type = "petit pony";
+}
+
+WrongAnimal::WrongAnimal(std::string typ)
+{
+	this->type = typ;
+}
+
+WrongAnimal&	WrongAnimal::operator=(const WrongAnimal cpy)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->type = cpy.type;
+	return (*this);
+}
+
+WrongAnimal::WrongAnimal(const WrongAnimal &cpy)
+{
+	std::cout << "copy constructor called" << std::endl;
+	*this = cpy;
+}
+
+WrongAnimal::~WrongAnimal()
+{
+}
---
+++ ex00/WrongCat.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongCat.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/18 08:18:33 by bde-wits          #+#    #+#             */
+/*   Updated: 2025/01/18 08:18:54 by bde-wits         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "WrongCat.hpp"
+
+WrongCat&	WrongCat::operator=(const WrongCat cpy)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->type = cpy.type;
+	return (*this);
+}
+
+WrongCat::WrongCat(const WrongAnimal &cpy)
+{
+	std::cout << "copy constructor called" << std::endl;
+	*this = cpy;
+}
+
+void	WrongCat::makeSound() const
+{
+	std::cout << "Wrong meow meow meow" << std::endl;
+	std::cout << "aka asoutaie pendant l'exam final" << std::endl;
+}
+
+WrongCat::WrongCat(std::string name) : WrongAnimal (name)
+{
+}
+
+WrongCat::WrongCat() : WrongAnimal("WrongCat")
+{
+	// this->type = "WrongCat";
+}
+
+WrongCat::~WrongCat()
+{
+}
