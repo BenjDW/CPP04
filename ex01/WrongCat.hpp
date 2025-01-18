15e17ac04a7332985ccbca36fafffc5216193931
vm_ubuntu42 <dewitskibenji@live.fr>
2025-01-18 08:25:12 -0800

    add makefile , and wrong*.cpp

--- ex01/WrongCat.hpp
+++ ex01/WrongCat.hpp
@@ -6,7 +6,7 @@
 /*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 04:33:40 by bde-wits          #+#    #+#             */
-/*   Updated: 2025/01/15 05:09:32 by bde-wits         ###   ########.fr       */
+/*   Updated: 2025/01/18 08:23:23 by bde-wits         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,36 +27,36 @@ class WrongCat : public WrongAnimal
 	private:
 };
 
-WrongCat&	WrongCat::operator=(const WrongCat cpy)
-{
-	std::cout << "Copy assignment operator called" << std::endl;
-	this->type = cpy.type;
-	return (*this);
-}
+// WrongCat&	WrongCat::operator=(const WrongCat cpy)
+// {
+// 	std::cout << "Copy assignment operator called" << std::endl;
+// 	this->type = cpy.type;
+// 	return (*this);
+// }
 
-WrongCat::WrongCat(const WrongAnimal &cpy)
-{
-	std::cout << "copy constructor called" << std::endl;
-	*this = cpy;
-}
+// WrongCat::WrongCat(const WrongAnimal &cpy)
+// {
+// 	std::cout << "copy constructor called" << std::endl;
+// 	*this = cpy;
+// }
 
-void	WrongCat::makeSound() const
-{
-	std::cout << "Wrong meow meow meow" << std::endl;
-	std::cout << "aka asoutaie pendant l'exam final" << std::endl;
-}
+// void	WrongCat::makeSound() const
+// {
+// 	std::cout << "Wrong meow meow meow" << std::endl;
+// 	std::cout << "aka asoutaie pendant l'exam final" << std::endl;
+// }
 
-WrongCat::WrongCat(std::string name) : WrongAnimal (name)
-{
-}
+// WrongCat::WrongCat(std::string name) : WrongAnimal (name)
+// {
+// }
 
-WrongCat::WrongCat() : WrongAnimal("WrongCat")
-{
-	// this->type = "WrongCat";
-}
+// WrongCat::WrongCat() : WrongAnimal("WrongCat")
+// {
+// 	// this->type = "WrongCat";
+// }
 
-WrongCat::~WrongCat()
-{
-}
+// WrongCat::~WrongCat()
+// {
+// }
 
 #endif
